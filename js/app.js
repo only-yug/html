d4f4be383e5444caf66fb2cb828d57d7bc1f41ec
only-yug <niharpolara9@gmail.com>
2025-02-07 18:17:57 +0530

    added the js file

---
+++ js/app.js
@@ -0,0 +1,50 @@
+console.log("Hello World");
+let a=100;
+let b=100;
+let c=30;
+console.log("The Sum Is",a+b);
+output=`The sum Is ${a+b}`;
+console.log(output);
+console.log('a=',a+' b=',b);
+console.log(`The sum Is ${a+b}`);
+console.log(`The ans of a>b`)
+console.log(a>b)
+console.log(`The ans of a>=b`)
+console.log(a>=b)
+console.log(`The ans of a<b`)
+console.log(a<b)
+console.log(`The ans of a<=b`)
+console.log(a<=b)
+console.log(`The ans of a==b`)
+console.log(a==b)
+console.log(`The ans of a!=b`)
+console.log(a!=b)
+
+let name='yug';
+let surmane='polara';
+console.log(`The ans of name == surname`)
+console.log( name == surmane )
+console.log(`The ans of name != surmane `)
+console.log( name != surmane )
+
+
+ //conditional statements
+
+ if(a!=b){
+    console.log("If statement Run SuccesFull")
+ }
+
+ if(name=="yug"){
+    console.log("Welcome Yug")
+ }
+
+ if(a>b){
+    if(a>c){
+        console.log("The A is the max number with the value of ",a)
+        
+    }
+    else 
+    console.log("The C is the max number with the value of ",c)
+ }
+ else if(b>c){console.log("The B is the max number with the value of ",b)}
+else console.log("The C is the max number with the value of ",c)
